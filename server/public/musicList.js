244debe963e1b68297eeadf320a3c45aeda2dfa3
Tierra Byrd <tierrabyrd1@gmail.com>
2024-05-08 15:33:07 -0500

    working on error messages

---
+++ server/public/musicList.js
@@ -0,0 +1,43 @@
+const artistListArray = [
+    {
+        name: 'Miles Davis',
+        born: 1926,
+        died: 1990,
+    },
+    {
+        name: 'Duke Ellington',
+        born: 1899,
+        died: 1974,
+    },
+    {
+        name: 'John Coltrane',
+        born: 1926,
+        died: 1987,
+    },
+    {
+        name: 'Louis Daniel Armstrong',
+        born: 1901,
+        died: 1971,
+    },
+];
+
+const songListArray = [
+    {
+        title: 'Take Five',
+        artist: 'The Dave Brubeck Quartet',
+    },
+    {
+        title: 'So What',
+        artist: 'Miles Davis',
+    },
+    {
+        title: 'Sing Sing Sing',
+        artist: 'Benny Goodman',
+    },
+    {
+        title: 'Take the "A" Train',
+        artist: 'The Dave Brubeck Quartet',
+    },
+];
+
+module.exports = musicList
--- server/server.js
+++ server/server.js
@@ -3,56 +3,44 @@ const express = require('express');
 const app = express();
 const PORT = 5001;
 
-const artistListArray = [
-    {
-        name: 'Miles Davis',
-        born: 1926,
-        died: 1990,
-    },
-    {
-        name: 'Duke Ellington',
-        born: 1899,
-        died: 1974,
-    },
-    {
-        name: 'John Coltrane',
-        born: 1926,
-        died: 1987,
-    },
-    {
-        name: 'Louis Daniel Armstrong',
-        born: 1901,
-        died: 1971,
-    },
-];
+const musicList =require('/musicList.js')
 
-const songListArray = [
-    {
-        title: 'Take Five',
-        artist: 'The Dave Brubeck Quartet',
-    },
-    {
-        title: 'So What',
-        artist: 'Miles Davis',
-    },
-    {
-        title: 'Sing Sing Sing',
-        artist: 'Benny Goodman',
-    },
-    {
-        title: 'Take the "A" Train',
-        artist: 'The Dave Brubeck Quartet',
-    },
-];
 
 app.use(express.static('server/public'));
 
+
+
 app.get('/artist', (req, res) => {
+  let artist = [];
     res.send(artistListArray);
-});
+
+  });
+
+  app.get('/artists', (req, res) => {
+    let artists = []
+  
+    //* Loop over quote and extract names, push to 'authors' array
+    for (let music of musicList) {
+      console.log("Current music", music)
+      artists.push(.author)
+    }
+  
+    console.log("Author list...", authors)
+  
+    // * Respond with authors data
+    res.send(authors)
+  })
 
 // TODO - Add GET for songs
 
+app.get('/title', (req, res)=> {
+    let titles = []
+    for (let music of musicList) {
+      console.log("Current artist", musicList)
+    titles.push(.title)}
+    
+    res.send(musicList)
+  })
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
